f6789b05d79113bba0f95103c9761288acd9e79e
Donavan Campbell <65779562+dangbroitsdon@users.noreply.github.com>
2025-10-15 16:26:32 -0400

    reduce further

--- update_packages.py
+++ update_packages.py
@@ -5,25 +5,22 @@ import subprocess
 from semantic_version import Version
 from specfile import Specfile
 
-def get_prefix(specfile_name: str):
-    return "mesa-" if specfile_name == "mesa" else "v"
-
 for i in os.scandir("specfiles/"):
     specfile = Specfile(i.path, autosave=True)
-    specfile_ver = specfile.version
-    specfile_url = specfile.url
     specfile_filename = os.fsdecode(i.name).split(".")[0]
+    specfile_url = specfile.url
+    specfile_ver = specfile.version
+    specfile_ver_prefix = "mesa-" if specfile_filename == "mesa" else "v"
 
     repo_tags = subprocess.check_output(f"./grab_git_repo_tags.sh {specfile_url}", shell=True, encoding="utf8").split()
-    repo_tag_regex = re.compile(rf"^{get_prefix(specfile_filename)}(\d+)\.(\d+)\.(\d+)$")
-    repo_tags = [repo_tag for repo_tag in repo_tags if repo_tag_regex.match(repo_tag) is not None]
+    repo_tags = [repo_tag for repo_tag in repo_tags if re.compile(rf"^{specfile_ver_prefix}(\d+)\.(\d+)\.(\d+)$").match(repo_tag) is not None]
 
-    latest_version = repo_tags[-1].removeprefix(get_prefix(specfile_filename))
+    latest_version = repo_tags[-1].removeprefix(specfile_ver_prefix)
 
-    if Version(latest_version) < Version(specfile_ver) :
-        print(f"no update available for {specfile_filename}")
+    if Version(latest_version) > Version(specfile_ver):
+        print(f"new update available for {specfile_filename}, latest version: {latest_version}, current version: {specfile_ver}")
+        specfile.version = repo_tags[-1]
+        subprocess.run(f"./start_copr_build.sh {specfile_filename}")
         continue
 
-    print(f"new update available for {specfile_filename}, version: {latest_version}")
-    specfile.version = repo_tags[-1]
-    os.system(f"./start_copr_build.sh {specfile_filename}")
+    print(f"no update available for {specfile_filename}")

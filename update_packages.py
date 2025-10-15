16fe4bb966f663dfea2609bb9bf2240589120c7f
Donavan Campbell <dangbroitsdon>
2025-10-15 16:12:11 -0400

    reduce

--- update_packages.py
+++ update_packages.py
@@ -8,27 +8,22 @@ from specfile import Specfile
 def get_prefix(specfile_name: str):
     return "mesa-" if specfile_name == "mesa" else "v"
 
-def populate_repo_tags(repo: str, specfile_name: str):
-    tags = subprocess.check_output(f"./grab_git_repo_tags.sh {repo}", shell=True, encoding="utf8").split()
-    repo_tag_regex = re.compile(rf"^{get_prefix(specfile_name)}(\d+)\.(\d+)\.(\d+)$")
-    tags = [tag for tag in tags if repo_tag_regex.match(tag) is not None]
-
-    return tags
-
 for i in os.scandir("specfiles/"):
     specfile = Specfile(i.path, autosave=True)
     specfile_ver = specfile.version
     specfile_url = specfile.url
+    specfile_filename = os.fsdecode(i.name).split(".")[0]
 
-    file_name = os.fsdecode(i.name).split(".")[0]
-    repo_tags = populate_repo_tags(specfile_url, file_name)
-    latest_version = repo_tags[-1].removeprefix(get_prefix(file_name))
+    repo_tags = subprocess.check_output(f"./grab_git_repo_tags.sh {specfile_url}", shell=True, encoding="utf8").split()
+    repo_tag_regex = re.compile(rf"^{get_prefix(specfile_filename)}(\d+)\.(\d+)\.(\d+)$")
+    repo_tags = [repo_tag for repo_tag in repo_tags if repo_tag_regex.match(repo_tag) is not None]
+
+    latest_version = repo_tags[-1].removeprefix(get_prefix(specfile_filename))
 
     if Version(latest_version) < Version(specfile_ver) :
-        print(f"no update available for {file_name}")
-        print(f"current version is {specfile_ver}")
+        print(f"no update available for {specfile_filename}")
         continue
 
-    print(f"new update available for {file_name}, version: {latest_version}")
+    print(f"new update available for {specfile_filename}, version: {latest_version}")
     specfile.version = repo_tags[-1]
-    os.system(f"./start_copr_build.sh {file_name}")
+    os.system(f"./start_copr_build.sh {specfile_filename}")
